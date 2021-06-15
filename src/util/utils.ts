074d0685803581e715e279ff7fa1b35a3a6ef877
menahem <mnjol770@gmail.com>
2021-06-15 10:42:02 +0300

    test: add test to diff func and make its wirk also for nested obj

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,15 +1,20 @@
 /**
+import { diff } from './utils';
  * Check the which fields has change
  * @param newObj obj to compare from
  * @param krtObj obj to compare to
  * @returns new obj with the fields that changed
  */
-export function diff<T>(newObj: T, krtObj: T) {
-  const diff: T = {} as T;
+export function diff<T>(newObj: T, krtObj: T): T {
+  const diffObj: T = {} as T;
 
   Object.keys(newObj).forEach((k) => {
-    if (newObj[k] !== krtObj[k]) diff[k] = newObj[k];
+    if (newObj[k] instanceof Object) {
+      diffObj[k] = diff(newObj[k], krtObj[k]);
+
+      if (Object.keys(diffObj[k]).length === 0) delete diffObj[k];
+    } else if (newObj[k] !== krtObj[k]) diffObj[k] = newObj[k];
   });
 
-  return diff;
+  return diffObj;
 }
