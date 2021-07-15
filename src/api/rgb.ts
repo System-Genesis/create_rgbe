dd918147937f66d640b2ce4ec629dd0e554e0650
770menahem <mnjol770@gmail.com>
2021-07-15 14:19:32 +0300

    feat: add spike to kartofel

--- src/api/rgb.ts
+++ src/api/rgb.ts
@@ -2,24 +2,32 @@ import axios from 'axios';
 import config from '../config/env.config';
 import { getResData } from './getResData';
 import { connectDiToEntityQueue } from '../rabbit/rabbit';
+import getToken from '../auth/spike';
 
 const api = config.krtflApi;
 
 export const ogApi = {
-  create: async (og: object) => await getResData(axios.post(`${api}/groups`, og)),
+  create: async (og: object) =>
+    await getResData(axios.post(`${api}/groups`, og, getToken())),
   get: async (hierarchy: string) => {
-    return await getResData(axios.get(`${api}/groups/${encodeURIComponent(hierarchy)}`));
+    return await getResData(
+      axios.get(`${api}/groups/${encodeURIComponent(hierarchy)}`, getToken())
+    );
   },
   update: async (id: string, og: object) => {
-    return await getResData(axios.patch(`${api}/groups/${id}`, og));
+    return await getResData(axios.patch(`${api}/groups/${id}`, og, getToken()));
   },
 };
 
 export const diApi = {
-  get: async (di: string) => await getResData(axios.get(`${api}/digitalIdentities/${di}`)),
-  create: async (di: object) => await getResData(axios.post(`${api}/digitalIdentities`, di)),
+  get: async (di: string) =>
+    await getResData(axios.get(`${api}/digitalIdentities/${di}`, getToken())),
+  create: async (di: object) =>
+    await getResData(axios.post(`${api}/digitalIdentities`, di, getToken())),
   update: async (id: string, di: object) => {
-    return await getResData(axios.patch(`${api}/digitalIdentities/${id}`, di));
+    return await getResData(
+      axios.patch(`${api}/digitalIdentities/${id}`, di, getToken())
+    );
   },
   connectToEntity: async (entityId: string, diId: string) => {
     // send to queue
@@ -29,27 +37,39 @@ export const diApi = {
 
 export const connectDiToEntityApi = async (entityId: string, diId: string) => {
   return await getResData(
-    axios.patch(`${api}/entities/${entityId}/connectDigitalIdentity`, {
-      digitalIdentityUniqueId: diId,
-    })
+    axios.patch(
+      `${api}/entities/${entityId}/connectDigitalIdentity`,
+      {
+        digitalIdentityUniqueId: diId,
+      },
+      getToken()
+    )
   );
 };
 
 export const roleApi = {
-  get: async (id: string) => await getResData(axios.get(`${api}/roles/${id}`)),
-  create: async (role: object) => await getResData(axios.post(`${api}/roles`, role)),
+  get: async (id: string) =>
+    await getResData(axios.get(`${api}/roles/${id}`, getToken())),
+  create: async (role: object) =>
+    await getResData(axios.post(`${api}/roles`, role, getToken())),
   update: async (id: string, role: object) =>
-    await getResData(axios.patch(`${api}/roles/${id}`, role)),
+    await getResData(axios.patch(`${api}/roles/${id}`, role, getToken())),
 
   connectToDI: async (roleId: string, digitalIdentityUniqueId: string) => {
     return await getResData(
-      axios.patch(`${api}/roles/${roleId}/connectDigitalIdentity`, {
-        digitalIdentityUniqueId,
-      })
+      axios.patch(
+        `${api}/roles/${roleId}/connectDigitalIdentity`,
+        {
+          digitalIdentityUniqueId,
+        },
+        getToken()
+      )
     );
   },
 
   connectToOG: async (id: string, groupId: string) => {
-    return await getResData(axios.patch(`${api}/roles/${id}/moveToGroup`, { groupId }));
+    return await getResData(
+      axios.patch(`${api}/roles/${id}/moveToGroup`, { groupId }, getToken())
+    );
   },
 };
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -10,10 +10,22 @@ export default {
     getRGB: env.get('GET_RGB_QUEUE').required().asString(),
     connectDiToEntity: env.get('CONNECT_D_E_QUEUE').required().asString(),
     retryOptions: {
-      minTimeout: env.get('RABBIT_RETRY_MIN_TIMEOUT').default(1000).asIntPositive(),
+      minTimeout: env
+        .get('RABBIT_RETRY_MIN_TIMEOUT')
+        .default(1000)
+        .asIntPositive(),
       retries: env.get('RABBIT_RETRY_RETRIES').default(2).asIntPositive(),
       factor: env.get('RABBIT_RETRY_FACTOR').default(1.8).asFloatPositive(),
     },
   },
   krtflApi: env.get('KRTFL_API').required().asString(),
+  spike: {
+    spikeUrl: env.get('SPIKE_URL').required().asString(),
+    redisUrl: env.get('REDIS_URL').required().asString(),
+    redisKeyName: env.get('REDIS_KEY_NAME').required().asString(),
+    myAud: env.get('MY_AUDIENCE').required().asString(),
+    clientId: env.get('MY_CLIENT_ID').required().asString(),
+    clientSecret: env.get('MY_CLIENT_SECRET').required().asString(),
+    kartofelAud: env.get('KARTOFEL_AUDIENCE').required().asString(),
+  },
 };
