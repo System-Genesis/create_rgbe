d93144277f27d2efb10eeb58e0e03f0840f60f0a
770menahem <mnjol770@gmail.com>
2021-08-17 10:38:19 +0300

    logWarn error response

--- src/api/getResData.ts
+++ src/api/getResData.ts
@@ -1,12 +1,12 @@
 import { AxiosResponse } from 'axios';
-import { logError } from './../logger/logger';
+import { logWarn } from './../logger/logger';
 
 export const getResData = async (req: Promise<AxiosResponse<any>>) => {
   try {
     const res = await req;
     return res.data;
   } catch (error) {
-    logError(`Response ${error.response.data}, status:  ${error.response.status}`, {
+    logWarn(`Response ${error.response?.data}, status:  ${error.response?.status}`, {
       url: error.config?.url,
       data: JSON.parse(error.config?.data || '{}'),
       msg: error.message,
