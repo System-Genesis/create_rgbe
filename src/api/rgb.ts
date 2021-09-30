f5175f58c80157aa5daa97140b5704b9655684ed
Roei <roeioren10@gmail.com>
2021-09-30 14:48:25 +0300

    update connerToEntity and move to group

--- src/api/rgb.ts
+++ src/api/rgb.ts
@@ -5,12 +5,16 @@ import { og, postOg } from '../types/rgbType';
 
 export const ogApi = {
   create: async (og: og) => {
-    const postOg: postOg = { name: og.name, source: og.source, directGroup: og.directGroup };
+    const postOg: postOg = {
+      name: og.name,
+      source: og.source,
+      directGroup: og.directGroup,
+    };
 
     return await getResData(axios.post(`/groups`, postOg));
   },
   get: async (hierarchy: string) => {
-    return await getResData(axios.get(`/groups/${encodeURIComponent(hierarchy)}`));
+    return await getResData(axios.get(`/groups/hierarchy/${encodeURIComponent(hierarchy)}`));
   },
   update: async (id: string, og: object) => {
     return await getResData(axios.patch(`/groups/${id}`, og));
@@ -23,18 +27,14 @@ export const diApi = {
   update: async (id: string, di: object) => {
     return await getResData(axios.patch(`/digitalIdentities/${id}`, di));
   },
-  connectToEntity: async (entityId: string, diId: string) => {
+  connectToEntity: async (entityIdentifier: string, diId: string) => {
     // send to queue
-    connectDiToEntity(entityId, diId);
+    connectDiToEntity(entityIdentifier, diId);
   },
 };
 
 export const connectDiToEntityApi = async (entityId: string, diId: string) => {
-  return await getResData(
-    axios.patch(`/entities/${entityId}/connectDigitalIdentity`, {
-      digitalIdentityUniqueId: diId,
-    })
-  );
+  return await getResData(axios.patch(`/entities/${entityId}/digitalIdentity/${diId}`));
 };
 
 export const roleApi = {
@@ -51,6 +51,6 @@ export const roleApi = {
   },
 
   connectToOG: async (id: string, groupId: string) => {
-    return await getResData(axios.patch(`/roles/${id}/moveToGroup`, { groupId }));
+    return await getResData(axios.patch(`/roles/${id}/group/${groupId}`));
   },
 };
