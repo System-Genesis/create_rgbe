379979b472ade9b8e77f6f30e9546e8b62e8f7b0
Roei <roeioren10@gmail.com>
2021-12-13 14:10:39 +0200

    feat: disconnectDi before push to redis

--- src/redis/connectDiToEntityRedis.ts
+++ src/redis/connectDiToEntityRedis.ts
@@ -4,36 +4,40 @@ import { connectDiToEntityApi, disconnectDiToEntityApi } from '../api/rgb';
 import { di } from '../types/rgbType';
 import { pushToArray, getArray, getAllKeys, delValue } from './redis';
 
-export const connectDiToEntity = async (entityIdentifier: string, di: di) => {
-  const entity = await entityApi.get(entityIdentifier);
+const disconnectDi = async (di: di, entityIdentifier: string) => {
+  if (!di.entityId) return;
 
-  if (entity) {
-    if (di.entityId) {
-      if (await disconnectDiToEntityApi(entity.id, di.uniqueId)) {
-        logger.info(false, 'APP', 'Entity disconnected from DI', `${entityIdentifier} disconnect from ${di.uniqueId}`, {
-          id: entityIdentifier,
-          uniqueId: di.uniqueId,
-        });
-      } else {
-        return logger.error(
-          false,
-          'APP',
-          'Entity fail to disconnected from DI',
-          `${entityIdentifier}  fail disconnect from ${di.uniqueId}`,
-          {
-            id: entityIdentifier,
-            uniqueId: di.uniqueId,
-          }
-        );
-      }
-    }
-    await connectDiToEntityApi(entity.id, di.uniqueId);
-    logger.info(false, 'APP', 'Entity connected to DI', `${entityIdentifier} connect to ${di.uniqueId}`, {
+  if (await disconnectDiToEntityApi(di.entityId, di.uniqueId)) {
+    logger.info(false, 'APP', 'Entity disconnected from DI', `${entityIdentifier} disconnect from ${di.uniqueId}`, {
       id: entityIdentifier,
       uniqueId: di.uniqueId,
     });
   } else {
-    pushToArray(entityIdentifier, di.uniqueId);
+    throw logger.error(false, 'APP', 'Entity fail to disconnected from DI', `${entityIdentifier}  fail disconnect from ${di.uniqueId}`, {
+      id: entityIdentifier,
+      uniqueId: di.uniqueId,
+    });
+  }
+};
+
+export const connectDiToEntity = async (entityIdentifier: string, di: di) => {
+  const entity = await entityApi.get(entityIdentifier);
+  try {
+    if (di.entityId) {
+      await disconnectDi(di, entityIdentifier);
+    }
+
+    if (entity) {
+      await connectDiToEntityApi(entity.id, di.uniqueId);
+      logger.info(false, 'APP', 'Entity connected to DI', `${entityIdentifier} connect to ${di.uniqueId}`, {
+        id: entityIdentifier,
+        uniqueId: di.uniqueId,
+      });
+    } else {
+      pushToArray(entityIdentifier, di.uniqueId);
+    }
+  } catch (error) {
+    console.log('disconnect failed');
   }
 };
 
@@ -60,7 +64,7 @@ export const runAll = async () => {
     const entityIdentifier = keys[i];
     const ent = await entityApi.get(entityIdentifier);
     if (ent) {
-      handleEntityEvent(entityIdentifier, ent.id || ent['_id']);
+      handleEntityEvent(entityIdentifier, ent.id);
     }
   }
 };
